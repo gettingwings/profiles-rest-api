b0d6b5f8de23e6335bbc76533c092bc2102068fd
gettingwings <malik.dipti@gmail.com>
2023-03-14 21:26:22 +0530

    added custom user profile, manager and migrations

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -1,3 +1,4 @@
+
 """profiles_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
--- profiles_project/wsgi.py
+++ profiles_project/wsgi.py
@@ -1,3 +1,4 @@
+
 """
 WSGI config for profiles_project project.
 
